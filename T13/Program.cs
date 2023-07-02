56344042fdf69b3d31e740bfdbd416654e870cce
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-02 14:54:08 +0300

    Add T13

---
+++ T13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе 
+// показывает вторую цифру этого числа
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number < 0)
+{
+    number *= -1;
+}
+
+if (number < 100) 
+{
+    Console.WriteLine("Третьей цифры  НЕТ");
+} 
+
+
+if (number >= 100)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+
+    int thiedDigit = number % 10;
+    Console.WriteLine($"Третья цифра: {thiedDigit}");
+}
