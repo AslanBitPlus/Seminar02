b16acc5f5c6b3d48d81b1138889539792854280d
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-02 12:32:56 +0300

    Chenged T11

--- T11/Program.cs
+++ T11/Program.cs
@@ -1,4 +1,4 @@
-﻿// Напишите программу, которая выводит случайное 
+﻿﻿// Напишите программу, которая выводит случайное 
 // трехзначное число и удаляет вторую цифру
 
 // 456 -> 46
@@ -8,11 +8,12 @@ int number = new Random().Next(100,1000); // 999 + 1
 Console.WriteLine($"Случайное трехзначное число -> {number}");
 
 int firstDigit = number / 100;
-int secondDigit = (number - firstDigit * 100) / 10;
-int thirdDigit = (number - firstDigit * 100 - secondDigit * 10);
+// int secondDigit = (number - firstDigit * 100) / 10;
+// int thirdDigit = (number - firstDigit * 100 - secondDigit * 10);
+int thirdDigit = number % 10;
 
 Console.WriteLine($"Первая цифра -> {firstDigit}");
-Console.WriteLine($"Вторая цифра -> {secondDigit}");
+// Console.WriteLine($"Вторая цифра -> {secondDigit}");
 Console.WriteLine($"Третья цифра -> {thirdDigit}");
 
 Console.WriteLine($"Ответ -> {firstDigit * 10 + thirdDigit}");
