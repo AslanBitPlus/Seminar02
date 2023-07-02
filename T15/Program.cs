57de25f966c7f484544a6f9d40a57ea52199587b
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-02 15:11:46 +0300

    Add T15

---
+++ T15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую
+// день недели, и проверяет, является ли этот день выходным
+
+
+Console.Write("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number < 1 || number > 7)
+{
+    Console.WriteLine($"Число: {number} не соответствует указанному диаппазону от 1 до 7");;
+}
+else
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+ 
