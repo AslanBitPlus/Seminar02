676436970cc2bc368168e37da3df36e42ad89276
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-02 11:46:41 +0300

    Add info

--- T09/Program.cs
+++ T09/Program.cs
@@ -1,2 +1,25 @@
-﻿// Напишите программу, которая выводит случайное число
+﻿
+// Напишите программу, которая выводит случайное число
 // из отрезка [10,99] и показывает наибольшую цифру
+
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+int number = 88; // new Random().Next(10,100); // 99 + 1
+Console.WriteLine($"Случайное число из диаппазанов 10 - 99 -> {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {firstDigit}");
+}
+else if (firstDigit < secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа: {secondDigit}");
+}
+else
+{
+    Console.WriteLine($"Цифры равны");
+}
