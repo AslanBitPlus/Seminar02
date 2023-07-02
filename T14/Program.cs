f996b83c04e73566405dd7811f808fc82ad7a705
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-02 13:29:59 +0300

    Add T14

---
+++ T14/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет принимать на вход число
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int firstNumber = 7;
+int secondNumber = 23;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// double multipFirstNumber = number % firstNumber;
+// double multipSecondNumber = number % secondNumber;
+
+
+if ((number % firstNumber == 0) & (number % secondNumber == 0))
+{
+    Console.WriteLine($"{number} кратно {firstNumber} и {secondNumber} одновременно");
+}
+else
+{
+    Console.WriteLine($"{number} одновременно не кратно {firstNumber} и {secondNumber}");
+}
+
